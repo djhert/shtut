3f910606ccdcd7cf0f7477593fb8a53143f4b91d
davidh <david.hertenstein@gmail.com>
2017-09-22 14:44:00 -0400

    working with webpage

--- tutorial.sh
+++ tutorial.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
 SHPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-alias hurl="\curl -f#LO"
+alias hurl="\curl --http1.1 -A "shtut" -f#LO"
 
 if [ "$1" == "setup" ]; then
 	printf "\nSetup\n\n"
 	printf "Downloading...\n"
-	mkdir -p "$SHPATH/core"
 	if [ $? != "0" ]; then
 		printf "Unable to create $SHPATH/core\nEnsure that $0 can write in this directory.\n\n"
 		exit 1
 	fi
-	source $(curl -s
 	exit 0
 fi
 
 if [ ! -d "$SHPATH/core" ]; then
-	printf "\nCore files are missing/corrupted for $0.\nPlease run \'$0 setup\'\n"
+	printf "\nCore files are missing for $0.\nPlease run \'$0 setup\'\n"
 	exit 1
 fi
 
